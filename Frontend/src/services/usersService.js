cca22ff792695354b4b8679bb4e2ff4002c847ae
Mohamed Mahdy <mohamedmahdy648@gmail.com>
2026-06-07 15:46:38 +0300

    Students endpoints, API integrations
    - create, update, Students
    - auth, academy, users, Services

---
+++ Frontend/src/services/usersService.js
@@ -0,0 +1,19 @@
+import api from "../api";
+
+export const getUsersRequest = () =>
+    api.get("api/users/");
+
+export const CreateUserRequest = (data) =>
+    api.get("api/users/", data);
+
+export const getUserRequest = (id) =>
+    api.get(`api/users/${id}`)
+
+export const updateUserRequest = (id, data) =>
+    api.put(`api/users/${id}`, data)
+
+export const deleteUserRequest = (id) =>
+    api.delete(`api/users/${id}`)
+
+export const createStuedetRequest = (data) =>
+    api.post("/api/auth/users/register/student/", data)
