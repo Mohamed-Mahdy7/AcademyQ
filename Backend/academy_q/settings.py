f713af38be47759a26f3d4cd5c7417885d216db8
Mohamed Mahdy <mohamedmahdy648@gmail.com>
2026-06-21 14:27:52 +0300

    Global DRF exception handler + standardized error contract

--- Backend/academy_q/settings.py
+++ Backend/academy_q/settings.py
@@ -31,7 +31,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    # 'rest_framework_simplejwt.token_blacklist',
     'djoser',
     'django_extensions',
     'corsheaders',
@@ -133,24 +132,20 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        'core.authentication.CookieJWTAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated'
-    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": ['core.authentication.CookieJWTAuthentication',],
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated'],
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    "EXCEPTION_HANDLER": "core.exceptions.custom_exception_handler",
     # 'DEFAULT_PAGINATION_CLASS': [
     #     'rest_framework.pagination.PageNumberPagination',
     #     'PAGE_SIZE': 10,
     # ]
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
-    # 'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('JWT',),
 }
 
