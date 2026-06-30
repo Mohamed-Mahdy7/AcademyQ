d359a298006d20bf5004cfec4b7a473632d6fcd0
Mohamed Mahdy <mohamedmahdy648@gmail.com>
2026-06-30 10:30:11 +0300

    email resend

--- Backend/academy_q/settings.py
+++ Backend/academy_q/settings.py
@@ -242,7 +242,7 @@ EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "False").lower() == "true"
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
 
 # celery
