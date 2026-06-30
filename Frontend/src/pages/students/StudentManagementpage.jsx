b6665efefba00171c92f33ab28a9e5d8fa846531
Mohamed Mahdy <mohamedmahdy648@gmail.com>
2026-06-30 11:37:25 +0300

    icons

--- Frontend/src/pages/students/StudentManagementpage.jsx
+++ Frontend/src/pages/students/StudentManagementpage.jsx
@@ -38,14 +38,35 @@ const StudentManagement = ({submit}) => {
                 <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 my-6">
                     <KpiCard
                         title={t("total_students")}
+                        svg={
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+                                <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"/>
+                                <circle cx="9" cy="7" r="4"/>
+                                <path d="M23 21v-2a4 4 0 0 0-3-3.87"/>
+                                <path d="M16 3.13a4 4 0 0 1 0 7.75"/>
+                            </svg>
+                        }
                         value={studentsCount}
                     />
                     <KpiCard
                         title={t("common:active")}
+                        svg={
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+                                <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"/>
+                                <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"/>
+                            </svg>
+                        }
                         value={activeStudents}
                     />
                     <KpiCard
                         title={t("with_overdue_payments")}
+                        svg={
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+                            <circle cx="12" cy="12" r="10"/>
+                            <line x1="12" y1="8" x2="12" y2="12"/>
+                            <line x1="12" y1="16" x2="12.01" y2="16"/>
+                            </svg>
+                        }
                         value={overdueStudents}
                     />
                 </div>
--- Frontend/src/pages/users/UserManagementPage.jsx
+++ Frontend/src/pages/users/UserManagementPage.jsx
@@ -33,9 +33,26 @@ const UserManagement = () => {
 
             <div className={showRegister ? "pointer-events-none" : ""}>
                 <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 my-6">
-                    <KpiCard title={t("total_staff").toUpperCase()} value={totalStaff} />
-                    <KpiCard title={t("admins").toUpperCase()} value={admins} />
-                    <KpiCard title={t("teachers").toUpperCase()} value={teachers} />
+                    <KpiCard title={t("total_staff").toUpperCase()} 
+                    svg={
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-cog w-4 h-4 text-blue"><circle cx="18" cy="15" r="3"></circle><circle cx="9" cy="7" r="4"></circle><path d="M10 15H6a4 4 0 0 0-4 4v2"></path><path d="m21.7 16.4-.9-.3"></path><path d="m15.2 13.9-.9-.3"></path><path d="m16.6 18.7.3-.9"></path><path d="m19.1 12.2.3-.9"></path><path d="m19.6 18.7-.4-1"></path><path d="m16.8 12.3-.4-1"></path><path d="m14.3 16.6 1-.4"></path><path d="m20.7 13.8 1-.4"></path></svg>
+                    }
+                    value={totalStaff} />
+                    <KpiCard title={t("admins").toUpperCase()} 
+                    svg={
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-shield w-4 h-4 text-blue"><path d="M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z"></path></svg>
+                    }
+                    value={admins} />
+                    <KpiCard title={t("teachers").toUpperCase()} 
+                    svg={
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+                            <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"/>
+                            <circle cx="9" cy="7" r="4"/>
+                            <path d="M23 21v-2a4 4 0 0 0-3-3.87"/>
+                            <path d="M16 3.13a4 4 0 0 1 0 7.75"/>
+                        </svg>
+                    }
+                    value={teachers} />
                 </div>
             </div>
 
